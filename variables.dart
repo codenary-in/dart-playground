4a51c1a7220fed0b911aa373ea124b7d8ff5a80c
Sagar Keshur <132813487+codenary-in@users.noreply.github.com>
2023-06-13 15:15:44 +0530

    Create variables.dart

---
+++ variables.dart
@@ -0,0 +1,96 @@
+void main() {
+// print("Hello coders");
+  
+  
+  //assign
+//   int amount;
+  
+  //assign
+//   amount = 1000;
+  
+// numbers = int, num, double 
+  //string = string 
+  
+  //boolen = bool = true,false;
+  
+  
+  
+  
+//   String name = 'codenary';
+  
+    
+//  name = name + " " + 'live';
+  
+//   print("Name : $name");
+  
+//   print('Bank balance : $amount');
+  
+  
+  
+//   bool isSelected = false;
+  
+//   isSelected = true;
+  
+//   print('Selected : $isSelected');
+  
+  
+
+ bool  is_selected_bank;
+  
+ bool isSelectedBank;
+  
+  
+//   List<int> students =[1,2,3,4,5,6,7];
+  
+//   students.add(8);
+  
+//   students.removeAt(3);
+  
+//   print('Total students = ${students.length}');
+  
+  
+ 
+  Map<int,String> values = {1:'sagar',2:'rahul',3:'ravi',4:'harry',5:'mohit'};
+  
+  
+  values.addAll({6:'rohit',7:'karan'});
+  
+  
+  
+  
+  print("Remove :  ${values.remove(3)}");
+  
+  
+  print("values ${values.length}: $values");
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+}
